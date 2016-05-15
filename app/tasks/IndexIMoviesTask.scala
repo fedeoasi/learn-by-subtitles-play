dc5ae44cbddc118a03013da7c564d8823474939e
fcaimi <fedeoasi@gmail.com>
2016-05-14 21:15:12 -0500

    Port IndexIMoviesTask from the deprecated scalatra version of learn by subtitles.

---
+++ app/tasks/IndexIMoviesTask.scala
@@ -0,0 +1,18 @@
+package tasks
+
+import persistence.ProdPersistenceManager
+import search.ElasticSearchInteractor
+import config.Config
+
+object IndexIMoviesTask {
+  def main(args: Array[String]): Unit = {
+    val persistenceManager = ProdPersistenceManager()
+    val searchInteractor = new ElasticSearchInteractor
+    searchInteractor.ensureMovieIndex(Config.movieIndexName)
+    val toBeIndexed = persistenceManager.listIMovies()
+    println(s"Found ${toBeIndexed.size} IMovies to index")
+    toBeIndexed.zipWithIndex.foreach {
+      case (m, i) => searchInteractor.indexIMovie(Config.movieIndexName, m, flush = false)
+    }
+  }
+}
