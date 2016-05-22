28c03c9681a4aa5da920e89819f54d6ba3054cd0
fcaimi <fedeoasi@gmail.com>
2016-05-22 18:50:27 -0500

    Less logging in the EpisodeDownloadingTask.

--- app/search/EpisodeDownloadingTask.scala
+++ app/search/EpisodeDownloadingTask.scala
@@ -13,10 +13,8 @@ class EpisodeDownloadingTask @Inject() (persistenceManager: PersistenceManager,
   val batchSize = 10
 
   def run() {
-    logger.info("Finding episodes to download")
     val toBeIndexed = persistenceManager.findEpisodesWithNoSubtitles().take(batchSize)
-    logger.info(s"Found ${toBeIndexed.size} episodes to download subtitles for")
-    logger.info(s"Files to be downloaded: $toBeIndexed")
+    logger.info(s"Files to download subtitles for: $toBeIndexed")
     toBeIndexed.foreach { s =>
       logger.info("Downloading subtitles for episode with imdbId: " + s.imdbID)
       subtitlesController.getSubtitlesForMovie(s.imdbID)
