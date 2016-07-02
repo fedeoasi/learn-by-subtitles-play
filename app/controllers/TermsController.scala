1cd3eaaf768191e30c8b2c79162a08e60a50aa73
fcaimi <fedeoasi@gmail.com>
2016-07-02 07:12:02 -0500

    Add API call to get a random term from the index.

--- app/controllers/TermsController.scala
+++ app/controllers/TermsController.scala
@@ -2,18 +2,30 @@ package controllers
 
 import javax.inject.Inject
 
+import akka.actor.ActorSystem
 import language.TermsProvider
 import play.api.mvc.{Action, Controller}
 import serialization.JsonFormats
 import org.json4s.jackson.Serialization._
 
-class TermsController @Inject() (termsProvider: TermsProvider)
+import scala.concurrent.ExecutionContext
+
+class TermsController @Inject() (actorSystem: ActorSystem,
+                                 termsProvider: TermsProvider)
+                                (implicit exec: ExecutionContext)
   extends Controller {
 
   implicit val formats = JsonFormats
 
-  def all = Action {
-    val terms = termsProvider.terms
-    Ok(write(terms)).as(JSON)
+  def all = Action.async {
+    termsProvider.allTerms.map { terms =>
+      Ok(write(terms)).as(JSON)
+    }
+  }
+
+  def random = Action.async {
+    termsProvider.randomTerm.map { term =>
+      Ok(write(term)).as(JSON)
+    }
   }
 }
--- app/language/TermsProvider.scala
+++ app/language/TermsProvider.scala
@@ -1,27 +1,45 @@
 package language
 
+import com.google.common.cache.CacheBuilder
 import com.google.inject.Inject
 import org.elasticsearch.search.aggregations.bucket.terms.{StringTerms, TermsBuilder}
 import search.ElasticSearchInteractor
-import scala.collection.JavaConverters._
 
+import scala.collection.JavaConverters._
+import scalacache._
+import guava._
+import memoization._
+import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.duration._
+import scala.util.Random
 
 case class Term(value: String, docCount: Long)
 
-class TermsProvider @Inject() () extends ElasticSearchInteractor {
+class TermsProvider @Inject() (implicit exec: ExecutionContext)
+  extends ElasticSearchInteractor {
+
   private[this] val TermsAggregationName = "text_terms"
 
   private[this] val Limit = 1000000
 
-  def terms: Seq[Term] = {
+  private[this] implicit val cache = ScalaCache(GuavaCache())
 
+  private[this] val random = new Random()
+
+  def allTerms: Future[Seq[Term]] = {
+    memoize(1.hour) {
+      Future.successful(extractTerms)
+    }
+  }
+
+  def extractTerms: Seq[Term] = {
     val search = client.prepareSearch("my-index")
       .setTypes("subtitle")
       .addField("_id")
       .addAggregation(new TermsBuilder(TermsAggregationName)
-          .field("text")
-          .size(1000000)
-    )
+        .field("text")
+        .size(1000000)
+      )
 
     val result = search.get()
 
@@ -34,5 +52,10 @@ class TermsProvider @Inject() () extends ElasticSearchInteractor {
     }
   }
 
-
+  def randomTerm: Future[Term] = {
+    allTerms.map { terms =>
+      val randomIndex = random.nextInt(terms.size)
+      terms(randomIndex)
+    }
+  }
 }
