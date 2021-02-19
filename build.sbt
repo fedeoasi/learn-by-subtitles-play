c8fb64975cbcb64d10fbe9f0c6eee20b6e8033b8
Federico Caimi <fedeoasi@gmail.com>
2021-02-19 08:27:24 -0600

    Restructure dependencies to simplify build.sbt

--- build.sbt
+++ build.sbt
@@ -6,48 +6,8 @@ lazy val root = (project in file(".")).enablePlugins(PlayScala)
 
 scalaVersion := "2.11.7"
 
-val joda = "joda-time" % "joda-time" % "2.4"
-val jodaConvert = "org.joda" % "joda-convert" % "1.7"
+import Dependencies._
 
-val webJars = {
-  val handlebars = "org.webjars.bower" % "handlebars" % "2.0.0"
-  val jQuery = "org.webjars" % "jquery" % "2.1.4"
-  val datatables = "org.webjars" % "datatables" % "1.10.9"
-  val datatablesTools = "org.webjars" % "datatables-tools" % "2.2.4-1"
-  val d3Cloud = "org.webjars" % "d3-cloud" % "1.0.5"
-  val colors = "org.webjars" % "colors" % "0.2.0"
-  Seq(handlebars, jQuery, datatables, datatablesTools, d3Cloud, colors)
-}
-
-lazy val flyway = "org.flywaydb" % "flyway-core" % "3.2.1"
-
-libraryDependencies ++= Seq(
-  jdbc,
-  cache,
-  ws,
-  flyway,
-  "org.apache.xmlrpc" % "xmlrpc" % "3.1.3",
-  "org.apache.xmlrpc" % "xmlrpc-client" % "3.1.3",
-  "org.apache.commons" % "commons-io" % "1.3.2",
-  "commons-lang" % "commons-lang" % "2.6",
-  "com.typesafe.slick" %% "slick" % "2.1.0",
-  "org.elasticsearch" % "elasticsearch" % "2.3.1",
-  "com.h2database" % "h2" % "1.4.181",
-  "org.xerial" % "sqlite-jdbc" % "3.8.7",
-  "com.google.guava" % "guava" % "19.0",
-  "mysql" % "mysql-connector-java" % "5.1.32",
-  "org.json4s" %% "json4s-jackson" % "3.2.11",
-  "org.json4s" %% "json4s-ext" % "3.2.11",
-  "org.codehaus.jackson" % "jackson-core-asl" % "1.9.11",
-  "net.databinder.dispatch" %% "dispatch-core" % "0.11.3",
-  "ch.qos.logback" % "logback-classic" % "1.0.13" % "runtime",
-  "com.github.cb372" %% "scalacache-core" % "0.9.1",
-  "com.github.cb372" %% "scalacache-guava" % "0.9.1",
-  joda,
-  jodaConvert,
-  "com.github.tototoshi" %% "scala-csv" % "1.3.6",
-  "com.jsuereth" %% "scala-arm" % "2.0",
-  "org.scalatestplus.play" %% "scalatestplus-play" % "1.5.0-RC1" % Test
-) ++ webJars
+libraryDependencies ++= allLibraryDependencies
 
 resolvers += "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases"
---
+++ project/Dependencies.scala
@@ -0,0 +1,72 @@
+import play.sbt.PlayImport.{cache, jdbc, ws}
+import sbt._
+
+object Dependencies {
+  object Versions {
+    val scalacache = "0.9.1"
+  }
+
+  private val commonsIo = "org.apache.commons" % "commons-io" % "1.3.2"
+  private val commonsLang = "commons-lang" % "commons-lang" % "2.6"
+  private val dispatch = "net.databinder.dispatch" %% "dispatch-core" % "0.11.3"
+  private val elasticSearch = "org.elasticsearch" % "elasticsearch" % "2.3.1"
+  private val flyway = "org.flywaydb" % "flyway-core" % "3.2.1"
+  private val guava = "com.google.guava" % "guava" % "19.0"
+  private val h2 = "com.h2database" % "h2" % "1.4.181" // should this be for tests only?
+  private val jacksonCoreAsl = "org.codehaus.jackson" % "jackson-core-asl" % "1.9.11"
+  private val joda = "joda-time" % "joda-time" % "2.4"
+  private val jodaConvert = "org.joda" % "joda-convert" % "1.7"
+  private val json4sJackson = "org.json4s" %% "json4s-jackson" % "3.2.11"
+  private val json4sExt = "org.json4s" %% "json4s-ext" % "3.2.11"
+  private val logback = "ch.qos.logback" % "logback-classic" % "1.0.13" % "runtime"
+  private val mySqlConnector = "mysql" % "mysql-connector-java" % "5.1.32"
+  private val scalacacheCore = "com.github.cb372" %% "scalacache-core" % Versions.scalacache
+  private val scalacacheGuava = "com.github.cb372" %% "scalacache-guava" % Versions.scalacache
+  private val scalaArm = "com.jsuereth" %% "scala-arm" % "2.0"
+  private val scalaCsv = "com.github.tototoshi" %% "scala-csv" % "1.3.6"
+  private val scalaTestPlay = "org.scalatestplus.play" %% "scalatestplus-play" % "1.5.0-RC1" % Test
+  private val slick = "com.typesafe.slick" %% "slick" % "2.1.0"
+  private val sqliteJdbc = "org.xerial" % "sqlite-jdbc" % "3.8.7"
+
+  private val webJars = {
+    val handlebars = "org.webjars.bower" % "handlebars" % "2.0.0"
+    val jQuery = "org.webjars" % "jquery" % "2.1.4"
+    val datatables = "org.webjars" % "datatables" % "1.10.9"
+    val datatablesTools = "org.webjars" % "datatables-tools" % "2.2.4-1"
+    val d3Cloud = "org.webjars" % "d3-cloud" % "1.0.5"
+    val colors = "org.webjars" % "colors" % "0.2.0"
+    Seq(handlebars, jQuery, datatables, datatablesTools, d3Cloud, colors)
+  }
+
+  private val xmlRpc = "org.apache.xmlrpc" % "xmlrpc" % "3.1.3"
+  private val xmlRpcClient = "org.apache.xmlrpc" % "xmlrpc-client" % "3.1.3"
+
+  val allLibraryDependencies: Seq[ModuleID] = Seq(
+    jdbc,
+    cache,
+    ws,
+    flyway,
+    xmlRpc,
+    xmlRpcClient,
+    commonsIo,
+    commonsLang,
+    slick,
+    elasticSearch,
+    h2,
+    sqliteJdbc,
+    guava,
+    mySqlConnector,
+    json4sJackson,
+    json4sExt,
+    jacksonCoreAsl,
+    dispatch,
+    logback,
+    scalacacheCore,
+    scalacacheGuava,
+    joda,
+    jodaConvert,
+    scalaCsv,
+    scalaArm,
+    scalaTestPlay
+  ) ++ webJars
+}
